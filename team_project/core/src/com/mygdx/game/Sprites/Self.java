54e009cbe90b9a4d2686efce8a92748438ba7fed
xido3947 <xido3947@colorado.edu>
2017-03-12 16:12:29 -0600

    Add files via upload

--- team_project/core/src/com/mygdx/game/Sprites/Self.java
+++ team_project/core/src/com/mygdx/game/Sprites/Self.java
@@ -3,7 +3,9 @@ package com.mygdx.game.Sprites;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.game.MyGdxGame;
 
 /**
  * Created by JonYoung on 3/5/17.
@@ -19,9 +21,17 @@ public class Self extends Sprite {
     }
     public void defineSelf() {
         BodyDef bdef = new BodyDef();
-        bdef.position.set(32, 32);
+        bdef.position.set(32 / MyGdxGame.PPM, 32 / MyGdxGame.PPM);
         bdef.type = BodyDef.BodyType.StaticBody;
         b2body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(5 / MyGdxGame.PPM);
+
+        fdef.shape = shape;
+        b2body.createFixture(fdef);
+
 
     }
 }
