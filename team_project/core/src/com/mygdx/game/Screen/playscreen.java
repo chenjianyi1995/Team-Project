2783cdc94687442736462645462b32061f8e0851
chenjianyi1995 <jich2029@colorado.edu>
2017-03-29 17:05:15 -0600

    fix more code

--- team_project/core/src/com/mygdx/game/Screen/playscreen.java
+++ team_project/core/src/com/mygdx/game/Screen/playscreen.java
@@ -28,6 +28,8 @@ import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.Scenes.Hud;
 import com.mygdx.game.Sprites.Bufflalo;
 
+
+
 /**
  * Created by Jianyi on 2/26/2017.
  */
@@ -48,63 +50,42 @@ public class playscreen implements Screen {
     private Box2DDebugRenderer b2dr;
 
     private Bufflalo player;
-
     public playscreen(Game game){
 
         this.game = (MyGdxGame) game;
 
         gamecam = new OrthographicCamera();
 
-        gamePort = new FitViewport(MyGdxGame.v_width / MyGdxGame.PPM, MyGdxGame.v_hieght,gamecam);
+        gamePort = new FitViewport(MyGdxGame.v_width / MyGdxGame.PPM, MyGdxGame.v_hieght/ MyGdxGame.PPM,gamecam );
         hud =  new Hud(((MyGdxGame) game).batch);
 
         mapLoader = new TmxMapLoader();
-        map = mapLoader.load("TMX.tmx");
+        map = mapLoader.load("Map simple.tmx");
         renderer = new OrthogonalTiledMapRenderer(map, 1 / MyGdxGame.PPM);
 
         gamecam.position.set(gamePort.getWorldWidth()/3, gamePort.getWorldHeight()/3,0);
         world = new World(new Vector2(0,0),true);
         b2dr = new Box2DDebugRenderer();
 
+        b2dr = new Box2DDebugRenderer();
+
+        player = new Bufflalo(world);
+
         BodyDef bdef = new BodyDef();
         PolygonShape shape = new PolygonShape();
         FixtureDef fdef = new FixtureDef();
         Body body;
 
-        //layer for river
+        //layer for obstacle
         for(MapObject object : map.getLayers().get(1).getObjects().getByType(RectangleMapObject.class)) {
             Rectangle rect = ((RectangleMapObject) object).getRectangle();
-            bdef.type = BodyDef.BodyType.StaticBody;
-            bdef.position.set(rect.getX() + rect.getWidth() / 3, rect.getY() + rect.getHeight()/3);
-            body = world.createBody(bdef);
-
-            shape.setAsBox(rect.getWidth() / 3, rect.getHeight() / 3);
-            fdef.shape = shape;
-            body.createFixture(fdef);
-        }
-        //layer for trees
-        for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
-            Rectangle rect = ((RectangleMapObject) object).getRectangle();
 
             bdef.type = BodyDef.BodyType.StaticBody;
-            bdef.position.set(rect.getX() + rect.getWidth() / 3, rect.getY() + rect.getHeight() / 3);
+            bdef.position.set((rect.getX() + rect.getWidth() / 2) / MyGdxGame.PPM, (rect.getY() + rect.getHeight() / 2)/ MyGdxGame.PPM);
 
             body = world.createBody(bdef);
 
-            shape.setAsBox(rect.getWidth() / 3, rect.getHeight() / 3);
-            fdef.shape = shape;
-            body.createFixture(fdef);
-        }
-        //layer for obstacle
-        for(MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
-            Rectangle rect = ((RectangleMapObject) object).getRectangle();
-
-            bdef.type = BodyDef.BodyType.StaticBody;
-            bdef.position.set(rect.getX() + rect.getWidth() / 3, rect.getY() + rect.getHeight() / 3);
-
-            body = world.createBody(bdef);
-
-            shape.setAsBox(rect.getWidth() / 3, rect.getHeight() / 3);
+            shape.setAsBox(rect.getWidth() / 2 / MyGdxGame.PPM, rect.getHeight() / 2 / MyGdxGame.PPM);
             fdef.shape = shape;
             body.createFixture(fdef);
         }
@@ -117,36 +98,35 @@ public class playscreen implements Screen {
     }
 
     public void handleInput(float dt){
-        if(Gdx.input.isKeyJustPressed(Input.Keys.UP))
-            player.b2body.applyLinearImpulse(new Vector2(0, 4f), player.b2body.getWorldCenter(), true);
         if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.b2body.getLinearVelocity().x <= 2)
             player.b2body.applyLinearImpulse(new Vector2(0.1f, 0), player.b2body.getWorldCenter(), true);
         if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && player.b2body.getLinearVelocity().x >= -2)
             player.b2body.applyLinearImpulse(new Vector2(-0.1f, 0), player.b2body.getWorldCenter(), true);
-        
+        if(Gdx.input.isKeyPressed(Input.Keys.UP) && player.b2body.getLinearVelocity().y <= 2)
+            player.b2body.applyLinearImpulse(new Vector2(0, 0.1f), player.b2body.getWorldCenter(), true);
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN) && player.b2body.getLinearVelocity().y >= -2)
+            player.b2body.applyLinearImpulse(new Vector2(0, -0.1f), player.b2body.getWorldCenter(), true);
     }
 
     public void update(float dt){
         handleInput(dt);
-        world.step(1/60f, 0,2);
+        world.step(1/60f, 6, 2);
 
-        gamecam.position.x = player.b2body. getPosition().x;
-
+        gamecam.position.x = player.b2body.getPosition().x;
+        gamecam.position.y = player.b2body.getPosition().y;
         gamecam.update();
         renderer.setView(gamecam);
     }
     @Override
     public void render(float delta) {
         update(delta);
-
-
         Gdx.gl.glClearColor(1,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         //render our game map
         renderer.render();
         b2dr.render(world, gamecam.combined);
         game.batch.setProjectionMatrix(gamecam.combined);
-        hud.stage.draw();;
+        hud.stage.draw();
     }
 
     @Override
--- team_project/core/src/com/mygdx/game/Sprites/Bufflalo.java
+++ team_project/core/src/com/mygdx/game/Sprites/Bufflalo.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.game.MyGdxGame;
 
 /**
  * Created by ALwyn on 2017/3/12.
@@ -21,14 +22,14 @@ public class Bufflalo extends Sprite{
     }
 
     public void defineBuffalo(){
-        BodyDef bdef =new BodyDef();
-        bdef.position.set(32,32);
+        BodyDef bdef = new BodyDef();
+        bdef.position.set(200 / MyGdxGame.PPM,200 / MyGdxGame.PPM);
         bdef.type = BodyDef.BodyType.DynamicBody;
         b2body = world.createBody(bdef);
 
         FixtureDef fedf= new FixtureDef();
         CircleShape shape = new CircleShape();
-        shape.setRadius(5);
+        shape.setRadius(5/ MyGdxGame.PPM);
 
         fedf.shape = shape;
         b2body.createFixture(fedf);
