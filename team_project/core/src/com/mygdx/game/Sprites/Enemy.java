e82382162cef99e4a205af333d950f7f61fa6873
zixu6769 <zixu6769@colorado.edu>
2017-03-29 18:02:42 -0600

    enemy

--- team_project/core/src/com/mygdx/game/Sprites/Enemy.java
+++ team_project/core/src/com/mygdx/game/Sprites/Enemy.java
@@ -1,9 +1,37 @@
 package com.mygdx.game.Sprites;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.game.MyGdxGame;
+
 /**
  * Created by JonYoung on 3/5/17.
  */
 
-public class Enemy {
+public class Enemy extends Sprite{
+    public World world;
+    public Body b2body;
 
+    public Enemy(World world){
+        this.world = world;
+        defineEnemy();
+    }
+
+    public void defineEnemy(){
+        BodyDef bdef = new BodyDef;
+        bdef.position.set(32 / MyGdxGame.PPM, 32 / MyGdxGame.PPM);
+        bdef.type = BodyDef.BodyType.DynamicBody;
+        b2body = world.createBody(bdef);
+
+        FixtureDef fdef = new FixtureDef();
+        CircleShape shape = new CircleShape();
+        shape.setRadius(5 / MyGdxGame.PPM);
+
+        fdef.shape = shape;
+        b2body.createFixture(fdef);
+    }
 }
