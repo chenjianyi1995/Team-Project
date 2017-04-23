803a3ec032124e2d2009ede9e6eb66c46474dbbc
Jonathan Young <jonathan.o.young@colorado.edu>
2017-04-23 17:20:33 -0600

    added bulletList to playscreen, added values to bullet sprite

--- team_project/core/src/com/mygdx/game/Sprites/Bullet.java
+++ team_project/core/src/com/mygdx/game/Sprites/Bullet.java
@@ -19,14 +19,21 @@ public class Bullet extends Sprite {
     public World world;
     public Body b2body;
 
-    public Bullet(World world){
+    private boolean vert;
+    private boolean horz;
+    private Bufflalo player;
+
+    public Bullet(World world, Bufflalo player, boolean vert, boolean horz){
         this.world = world;
+        this.player = player;
+        this.vert = vert;
+        this.horz = horz;
         defineBullet();
     }
 
     private void defineBullet() {
         BodyDef bdef = new BodyDef();
-        bdef.position.set(200 / MyGdxGame.PPM,200 / MyGdxGame.PPM);
+        bdef.position.set(player.b2body.getPosition().x / MyGdxGame.PPM,player.b2body.getPosition().y / MyGdxGame.PPM);
         bdef.type = BodyDef.BodyType.DynamicBody;
         b2body = world.createBody(bdef);
 
