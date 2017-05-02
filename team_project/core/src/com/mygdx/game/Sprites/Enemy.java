5668e984419bdd60197e848a1a4daa9141ad6c35
Zijun Xu <zixu6769@colorado.edu>
2017-05-02 14:37:47 -0600

    Ram can now move to random direction, but still need to fix collision.

--- team_project/core/src/com/mygdx/game/Sprites/Enemy.java
+++ team_project/core/src/com/mygdx/game/Sprites/Enemy.java
@@ -2,6 +2,7 @@ package com.mygdx.game.Sprites;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
@@ -10,7 +11,7 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.Screen.playscreen;
 import com.mygdx.game.Scenes.Hud;
-
+import java.util.Random;
 /**
  * Created by JonYoung on 3/5/17.
  */
@@ -25,7 +26,8 @@ public class Enemy extends Sprite{
     private static int level;
     private static int damage;
     private static int movSpd;
-
+    Random rand = new Random();
+    public Vector2 velocity;
     public Enemy(playscreen screen, float x, float y){
        // super(screen.getAtlas().findRegion("RAM"));
 
@@ -35,6 +37,24 @@ public class Enemy extends Sprite{
         ramstand = new TextureRegion(screen.getAtlas().findRegion("RAM"), 0, 0, 14, 16);
         setBounds(0,0,14/MyGdxGame.PPM,16/MyGdxGame.PPM);
         setRegion(ramstand);
+            int n = rand.nextInt(8) + 1;
+            if (n == 1) {
+                velocity = new Vector2(1, 0);
+            } else if (n == 2) {
+                velocity = new Vector2(-1, 0);
+            } else if (n == 3) {
+                velocity = new Vector2(0, 1);
+            } else if (n == 4) {
+                velocity = new Vector2(0, -1);
+            } else if (n == 5) {
+                velocity = new Vector2(1, 1);
+            } else if (n == 6) {
+                velocity = new Vector2(-1,-1);
+            } else if (n == 7) {
+                velocity = new Vector2(1, -1);
+            } else if (n == 8) {
+                velocity = new Vector2(-1, 1);
+            }
     }
 
     public Enemy(Body b2body) {
@@ -67,6 +87,7 @@ public class Enemy extends Sprite{
     public void update(float dt){
         setPosition(b2body.getPosition().x - getWidth() /2 , b2body.getPosition().y - getHeight()/2);
         setLevel(dt);
+        b2body.setLinearVelocity(velocity);
     }
 
     public static int getLevel() {
